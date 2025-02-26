c7bce1d92db3f14dee87abb02dd6681483acc118
dinoDuvnjak <dino.duvnjak24@gmail.com>
2025-02-26 12:16:33 +0100

    Initial setup

---
+++ TypewritterTextTest/Source/TypewritterTextTest/TypewritterTextTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TypewritterTextTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TypewritterTextTest, "TypewritterTextTest" );
